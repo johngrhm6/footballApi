24580d2f75f69df9007d1b9d58caf3d5beebedce
johngraham <johngrhm6@gmail.com>
2021-05-06 23:17:50 +0100

    fixed mobile menu

--- footballApi/static/footballApi/js/main.js
+++ footballApi/static/footballApi/js/main.js
@@ -1,24 +1,7 @@
-function sortTable () {
-    var table ,rows,switching,i,x,y, shouldSwitch;
-    table = document.getElementsByClassName("matches");
-    switching=true;
-    while (switching){
-        switching=false;
-        rows = table.rows;
-        for (i=1;i<(rows.length-1);i++){
-            shouldSwitch =false;
-            x = rows[i].getElementByTagName('TD')[0];
-            Y = rows[i+1].getElementByTagName('TD')[0];
-            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()){
-                shouldSwitch =true;
-                break;
-            }
-        }
-        if(shouldSwitch){
-            rows[i].parentNode.insertBefore(rows[i+1],rows[i]);
-            switching=true;
-        }
-        
-    }
+M.AutoInit();
 
-}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
